5fa1c509668b49528c263cb0117686900b8d797f
Brian Yang <b.y.yang212@gmail.com>
2024-08-09 19:48:07 -0400

    Change colors to match game and add outline on hover to large champion cards

--- src/components/SelectedChampionButton.tsx
+++ src/components/SelectedChampionButton.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@mantine/core';
 import { useSelectedTeam } from './contexts/SelectedTeamContext';
 import { TierToColorMap } from '@/utils/TiersUtils';
+import { useState } from 'react';
 
 interface SelectedChampionButtonProps {
   champion: Champion;
@@ -12,19 +13,26 @@ const circleButtonStyles = (
   width: number,
   height: number,
   tier: number,
-  imageUrl: string
+  imageUrl: string,
+  isHovered: boolean
 ) => ({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
   borderStyle: 'solid',
   borderWidth: '0px',
+  outline: isHovered ? `4px solid ${TierToColorMap[tier]}` : '',
   borderColor: TierToColorMap[tier],
   width: width,
   height: height,
   overflow: 'hidden',
-  backgroundImage: `url(${imageUrl})`,
+  backgroundImage: `${
+    isHovered
+      ? 'radial-gradient(circle, rgba(0,0,0,0) 25%, rgba(100,255,255,0.5) 75%, rgba(150,150,150,0.8) 100%),'
+      : ''
+  } url(${imageUrl})`,
   backgroundSize: '100%',
+  cursor: 'pointer',
 });
 
 const SelectedChampionButton: React.FC<SelectedChampionButtonProps> = ({
@@ -33,14 +41,18 @@ const SelectedChampionButton: React.FC<SelectedChampionButtonProps> = ({
   height,
 }) => {
   const { toggleChampion } = useSelectedTeam();
+  const [isHovered, setIsHovered] = useState<boolean>(false);
   return (
     <Button
       style={circleButtonStyles(
         width,
         height,
         champion.tier,
-        champion.splashPath
+        champion.splashPath,
+        isHovered
       )}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
       onClick={() => toggleChampion(champion)}
     />
   );
--- src/utils/TiersUtils.tsx
+++ src/utils/TiersUtils.tsx
@@ -1,9 +1,9 @@
 export const TIERS = [1, 2, 3, 4, 5];
 
 export const TierToColorMap: { [key: number]: string } = {
-  1: 'grey',
-  2: 'green',
-  3: 'blue',
-  4: 'purple',
-  5: 'yellow',
+  1: '#78746e',
+  2: '#0fb740',
+  3: '#4ea0f9',
+  4: '#e649f5',
+  5: '#f7b720',
 };
